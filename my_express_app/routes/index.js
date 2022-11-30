74ca6eea8862a30b5f2e6da41a66d20e4841b269
Rojlex <blablabla20040116@mail.ru>
2022-11-30 19:41:24 +0300

    4.2

--- my_express_app/routes/index.js
+++ my_express_app/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Карамельки */
+router.get('/Priora', function(req, res, next) {
+  res.send("<h1>Страница Приоры</h1>")
+});
+
+
 module.exports = router;
